7b587ca2fb98758b4bf7cc6ba4738174ec712162
burnysc2 <gamingburny@gmail.com>
2026-02-23 15:56:40 +0100

    Add url input

--- src/routes/video-player/+page.svelte
+++ src/routes/video-player/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 // TODO Implement video player with control buttons
 
+let video_url = $state("")
+
 // normal: show meta info below player, enable scroll
 // theater:
 // fullscreen: only show video unless hovering with mouse in video player, then temporarily (debounce) show controls
@@ -26,9 +28,20 @@ function set_playback_speed(new_playback_speed: number) {}
 function toggle_fullscreen() {}
 </script>
 
-
-<div>
+<div class="flex flex-col items-center w-screen">
+	<input class="m-2 p-2 border" bind:value={video_url} placeholder="Enter video url" />
     <!-- TODO Implement video player -->
+	{#if video_url}
+		<video
+			controls
+			muted
+			playsinline
+			src={video_url}
+			style="border-radius: 8px;"
+		>
+			Your browser does not support the video tag.
+		</video>
+	{/if}
 </div>
 
 
