41e5ca7dad0292a466e6e30ddde6bfed1f58d193
burnysc2 <gamingburny@gmail.com>
2026-02-25 23:58:35 +0100

    Fix playback sync

--- src/lib/peer_handling/peer_setup_connection.svelte.ts
+++ src/lib/peer_handling/peer_setup_connection.svelte.ts
@@ -13,7 +13,8 @@ export function setup_connection(peer: Peer, conn: DataConnection, options: TSet
 				peer_ids: temp_state.peer_connections.map((c) => c.peer),
 				playlist: temp_state.playlist,
 				playlist_index: temp_state.playlist_index,
-				video_target_playback_speed: temp_state.video_target_playback_speed,
+				// TODO Use video_target_playback_speed when catch-up is implemented
+				video_target_playback_speed: temp_state.video_playback_speed,
 				video_current_time: temp_state.video_current_time,
 				video_state_paused: temp_state.video_state_paused,
 			})
