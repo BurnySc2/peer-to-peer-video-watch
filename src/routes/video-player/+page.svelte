bd26412a6034bcc347e1b54e3a66091c354cd9fa
Zithr <zithersorrow@gmail.com>
2026-02-26 18:17:36 +0000

    Add max-w

--- src/routes/video-player/+page.svelte
+++ src/routes/video-player/+page.svelte
@@ -3,7 +3,7 @@ import PlaybackControls from "$lib/components/ui/PlaybackControls.svelte"
 import VideoPlayer from "$lib/components/ui/VideoPlayer.svelte"
 </script>
 
-<div class="flex flex-col items-center w-screen p-4 space-y-2">
+<div class="flex flex-col max-w-screen items-center p-4 space-y-2">
 	<VideoPlayer />
 	<PlaybackControls />
 </div>
