d4afa40a6062ec70cb8b25f7d2a5fadaa4d44425
Zithr <zithersorrow@gmail.com>
2026-02-26 02:20:41 +0000

    Add seek hover tooltip

--- src/lib/components/ui/NewControls.svelte
+++ src/lib/components/ui/NewControls.svelte
@@ -57,14 +57,27 @@ function seek_forward() {
 function seek_back() {
 	seek_to_time(temp_state.video_current_time - 10)
 }
+
+let seek_hover_value = $state<string | null>(null)
+let seek_hover_percent = $state(0)
+
+function handle_seek_hover(event: MouseEvent) {
+	const target = event.currentTarget as HTMLElement
+	const rect = target.getBoundingClientRect()
+
+	let percent = (event.clientX - rect.left) / rect.width
+
+	seek_hover_value = format_time(percent * temp_state.video_duration)
+	seek_hover_percent = percent * 100
+}
 </script>
 
 <div class="absolute bottom-0 left-0 right-0 bg-black/60 p-2 flex gap-2 w-full transition-opacity duration-500" style="opacity: {controls_opacity};">
     <button onclick={local_set_play_pause}>
         {#if temp_state.video_state_paused}
-        <PlayIcon />
+            <PlayIcon />
         {:else}
-        <PauseIcon />
+            <PauseIcon />
         {/if}
     </button>
     <button onclick={seek_back}>
@@ -74,18 +87,28 @@ function seek_back() {
         <ForwardIcon />
     </button>
     <div id="current-time" class="text-white select-none min-w-14 max-w-14">{current_time_formatted}</div>
-    <input
-        type="range"
-        class="w-full mx-8"
-        min="0"
-        max={temp_state.video_duration}
-        step="0.01"
-        value={temp_state.video_current_time || 0}
-        oninput={(e) => {
-            // @ts-ignore            
-            seek_to_time(Number.parseFloat(e.target.value))
-        }}
-    />
+    <div class="relative w-full mx-8" role="presentation" onmousemove={handle_seek_hover} onmouseleave={() => seek_hover_value = null}>
+        <input
+            type="range"
+            class="w-full "
+            min="0"
+            max={temp_state.video_duration}
+            step="0.01"
+            value={temp_state.video_current_time || 0}
+            oninput={(e) => {
+                // @ts-ignore            
+                seek_to_time(Number.parseFloat(e.target.value))
+            }}
+        />
+        {#if seek_hover_value !== null}
+            <div
+				class="absolute -top-6 -translate-x-1/2 bg-black text-white text-xs px-2 py-1 rounded pointer-events-none"
+				style="left: {seek_hover_percent}%"
+			>
+				{seek_hover_value}
+			</div>
+        {/if}
+    </div>
     <div id="remaining-time" class="text-white select-none min-w-14 max-w-14 text-right">{current_remaining_time}</div>
     <div id="time-separator" class="text-white select-none mx-[-8px]">|</div>
     <div id="total-time" class="text-white select-none min-w-14 max-w-14">{total_time}</div>
