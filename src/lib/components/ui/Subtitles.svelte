d96a54501058494f3fbb8c18b739640531ba4dad
Zithr <zithersorrow@gmail.com>
2026-03-05 19:43:42 +0000

    Fix subtitle fading

--- src/lib/components/ui/Subtitles.svelte
+++ src/lib/components/ui/Subtitles.svelte
@@ -7,10 +7,9 @@ interface Props {
 let { subtitle_text, enabled }: Props = $props()
 </script>
 
-{#if subtitle_text && enabled}
-    <div
-        class="absolute bottom-10 left-0 w-full text-center text-lg pointer-events-none transition-opacity duration-150"
-    >
-        <div class="inline-block p-2 bg-black/70 rounded-lg">{subtitle_text}</div>
-    </div>
-{/if}
+<div
+    class="absolute bottom-10 left-0 w-full text-center text-lg pointer-events-none transition-opacity duration-150"
+    style="opacity: {subtitle_text && enabled ? 1 : 0}"
+>
+    <div class="inline-block p-2 bg-black/70 rounded-lg">{subtitle_text}</div>
+</div>
