3f8f832f672319177cc7ea0bb05320a319d63f3e
Zithr <zithersorrow@gmail.com>
2026-03-07 04:09:02 +0000

    Add random motion to emotes

--- src/lib/components/ui/Emotes.svelte
+++ src/lib/components/ui/Emotes.svelte
@@ -5,7 +5,7 @@ import { perma_state } from "$lib/persistent-storage.svelte"
 import { temp_state } from "$lib/temporary-storage.svelte"
 import { emote_state, emotes } from "$lib/utils/emotes.svelte"
 
-const SEND_EMOTE_COOLDOWN_MS = 2000
+const SEND_EMOTE_COOLDOWN_MS = 100
 
 interface Props {
     controls_opacity: number
@@ -25,7 +25,7 @@ function display_emote(emote: string) {
     console.log(emote)
     if (!allow_emote_push) return
     allow_emote_push = false
-    show_emote_list = false
+    // show_emote_list = false
     const id = generate_emote_id()
 
     emote_state.push({ id, src: emote })
@@ -39,8 +39,14 @@ function display_emote(emote: string) {
     }, SEND_EMOTE_COOLDOWN_MS)
 }
 
-function generate_emote_id() {
+function generate_emote_id(): string {
     return `${Date.now()}-${Math.floor(Math.random() * 10000)}`
+}
+
+function random_helper(a: number, b: number): number {
+    const min = Math.min(a, b)
+    const max = Math.max(a, b)
+    return Math.random() * (max - min) + min
 }
 
 $effect(() => {
@@ -49,10 +55,17 @@ $effect(() => {
 </script>
 
 {#each emote_state as emote (emote.id)}
-    <div class="absolute bottom-0 right-0 w-20 h-20 flex items-center justify-center">
+    {@const rise = random_helper(5,7)}
+    {@const wobble = random_helper(1.5,2.5)}
+    {@const x = random_helper(-80,0)}
+    <div
+        class="absolute bottom-0 right-0 w-24 h-24 flex items-center justify-center"
+        style="transform: translateX({x}px);"
+    >
         <img
             src={emote.src}
-            class="max-w-full max-h-full object-contain pointer-events-none emote-rise"
+            class="max-w-3/4 max-h-3/4 object-contain drop-shadow pointer-events-none emote-rise"
+            style="--rise-time:{rise}s; --wobble-time:{wobble}s;"
             alt=""
         >
     </div>
@@ -105,7 +118,7 @@ $effect(() => {
         </div>
     {:else}
         <button
-            class="absolute top-1/2 right-0 mr-1 bg-black/60 text-white hover:text-blue-400 hover:scale-130 transition-opacity duration-500"
+            class="absolute top-1/2 right-0 mr-1 p-1 bg-black/60 rounded-xl text-white hover:text-blue-400 hover:scale-130 transition-opacity duration-500"
             style="opacity: {controls_opacity};"
             onclick={() => {show_emote_list = !show_emote_list}}
         >
@@ -140,9 +153,9 @@ $effect(() => {
 
 .emote-rise {
     offset-path: path("M0,0 C 40,-120 -40,-240 20,-360 C -20,-480 20,-600 0,-720");
-    animation:
-        emote-rise 6s linear forwards,
-        emote-wobble 2s ease-in-out infinite;
     offset-rotate: 0deg;
+    animation:
+        emote-rise var(--rise-time, 6s) linear forwards,
+        emote-wobble var(--wobble-time, 2s) ease-in-out infinite;
 }
 </style>
