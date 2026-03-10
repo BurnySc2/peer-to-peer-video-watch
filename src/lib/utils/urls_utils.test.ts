e7a48d33e4b6779f57e26f357ec4b64a2a2f8362
Zithr <zithersorrow@gmail.com>
2026-03-10 00:16:03 +0000

    Add urls_utils test

---
+++ src/lib/utils/urls_utils.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it } from "vitest"
+import { get_search_params, is_valid_url } from "./url_utils"
+
+describe("is_valid_url", () => {
+    it.each([
+        ["sometext", false],
+        ["httpsometext.com", false],
+        ["https://example.com", true],
+        ["http://localhost:8000/video.mp4", true],
+    ])("returns %s -> %s", (input, expected) => {
+        expect(is_valid_url(input)).toBe(expected)
+    })
+})
+
+describe("get_search_params", () => {
+    it.each([
+        ["https://example.com", "https://example.com", "/", {}],
+        [
+            "http://localhost:8000/room?room_id=78371f61-e8de-4fb4-b3ad",
+            "http://localhost:8000",
+            "/room",
+            { room_id: "78371f61-e8de-4fb4-b3ad" },
+        ],
+        [
+            "https://examplesite.com/room?room_id=78371f61-e8de-4fb4-b3ad&peer_id=2938659-b324-6354-l2",
+            "https://examplesite.com",
+            "/room",
+            {
+                room_id: "78371f61-e8de-4fb4-b3ad",
+                peer_id: "2938659-b324-6354-l2",
+            },
+        ],
+    ])("parses %s", (input, expected_origin, expected_path, expected_params) => {
+        const [url, params] = get_search_params(input)
+
+        expect(url.origin).toBe(expected_origin)
+        expect(url.pathname).toBe(expected_path)
+        expect(params).toStrictEqual(expected_params)
+    })
+})
