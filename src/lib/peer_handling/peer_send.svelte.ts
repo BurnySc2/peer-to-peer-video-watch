2fad4144232d17e0dd3748f90385a4308f5d2ae0
Zithr <zithersorrow@gmail.com>
2026-03-15 22:35:22 +0000

    Improve logs

--- src/lib/peer_handling/peer_send.svelte.ts
+++ src/lib/peer_handling/peer_send.svelte.ts
@@ -3,6 +3,7 @@ import { perma_state } from "$lib/persistent-storage.svelte"
 import { temp_state } from "$lib/temporary-storage.svelte"
 import type { TMessage } from "$lib/types/peer_to_peer"
 import type { TPlayListItem } from "$lib/temporary-storage.svelte"
+import { format_time } from "$lib/utils/format_time"
 
 export function broadcast(data: TMessage) {
     temp_state.peer_connections.forEach((conn) => {
@@ -30,11 +31,11 @@ export function p2p_send_video_pause(time: number) {
 }
 export function p2p_send_video_seek_to(time: number) {
     broadcast({ type: "video_seek_to", time: time })
-    console.log("broadcasting seek to")
+    console.log("broadcasting seek to ", format_time(time))
 }
 export function p2p_send_video_set_playback_rate(message: { time: number; value: number }) {
     broadcast({ type: "video_set_playback_rate", time: message.time, value: message.value })
-    console.log("broadcasting playback rate")
+    console.log("broadcasting playback rate ", message.value)
 }
 export function p2p_video_current_time_sync(message: { time: number }) {
     broadcast({ type: "video_current_time_interval", time: message.time, timestamp_now: Date.now() })
@@ -59,7 +60,7 @@ export function p2p_send_ready() {
 }
 export function p2p_send_subtitle_offset() {
     broadcast({ type: "send_subtitle_offset", subtitle_offset: temp_state.subtitles.offset })
-    console.log("broadcasting subtitle_offset")
+    console.log("broadcasting subtitle_offset ", temp_state.subtitles.offset)
 }
 export function p2p_send_emote(id: string, emote: string) {
     broadcast({ type: "send_emote", id, emote })
