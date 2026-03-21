e0f25d1b11fc2a48fb208b6b46b2ad830f0d7f44
Zithr <zithersorrow@gmail.com>
2026-03-21 17:38:22 +0000

    Add reconnecting test for member and host

--- e2e/video-player.test.ts
+++ e2e/video-player.test.ts
@@ -337,3 +337,83 @@ test("p2p sync controls", async ({ browser }) => {
             s2: "1",
         })
 })
+
+test("p2p member reconnects", async( { browser }) => {
+    const { host_page, member_page } = await setup_p2p_room(browser)
+    await add_video_to_playlist(host_page, TEST_VIDEO_1)
+    const video_host = host_page.locator("video")
+    const video_member = member_page.locator("video")
+
+    await expect(video_host).toHaveJSProperty("paused", true)
+    await expect(video_member).toHaveJSProperty("paused", true)
+
+    // Seek video to 30s
+    const target = 30
+    const slider = host_page.getByTestId("seek-slider")
+    await slider.evaluate((el, value) => {
+        const input = el as HTMLInputElement
+        input.valueAsNumber = value
+        input.dispatchEvent(new InputEvent("input", { bubbles: true }))
+    }, target)
+
+    // Host and member both seek correctly
+    await expect
+    .poll(async () => await video_host.evaluate((v: HTMLVideoElement) => v.currentTime))
+    .toBeGreaterThanOrEqual(target)
+
+    await expect
+    .poll(async () => await video_member.evaluate((v: HTMLVideoElement) => v.currentTime))
+    .toBeGreaterThanOrEqual(target)
+
+    // Member refreshes page (waituntil: networkidle - might not work here since video is loading)
+    await member_page.reload({waitUntil: "load"})
+
+    // Members reloads video at correct time
+    await expect
+    .poll(async () => await video_member.evaluate((v: HTMLVideoElement) => v.currentTime))
+    .toBeGreaterThanOrEqual(target)
+
+    // Member is still part of the group - host presses play, member video plays
+    await host_page.getByTestId("player-play").click()
+    await expect(video_member).toHaveJSProperty("paused", false)
+})
+
+test("p2p host reconnects", async( { browser }) => {
+    const { host_page, member_page } = await setup_p2p_room(browser)
+    await add_video_to_playlist(host_page, TEST_VIDEO_1)
+    const video_host = host_page.locator("video")
+    const video_member = member_page.locator("video")
+
+    await expect(video_host).toHaveJSProperty("paused", true)
+    await expect(video_member).toHaveJSProperty("paused", true)
+
+    // Seek video to 30s
+    const target = 30
+    const slider = host_page.getByTestId("seek-slider")
+    await slider.evaluate((el, value) => {
+        const input = el as HTMLInputElement
+        input.valueAsNumber = value
+        input.dispatchEvent(new InputEvent("input", { bubbles: true }))
+    }, target)
+
+    // Host and member both seek correctly
+    await expect
+    .poll(async () => await video_host.evaluate((v: HTMLVideoElement) => v.currentTime))
+    .toBeGreaterThanOrEqual(target)
+
+    await expect
+    .poll(async () => await video_member.evaluate((v: HTMLVideoElement) => v.currentTime))
+    .toBeGreaterThanOrEqual(target)
+
+    // Host refreshes page (waituntil: networkidle - might not work here since video is loading)
+    await host_page.reload({waitUntil: "load"})
+
+    // Host reloads video at correct time
+    await expect
+    .poll(async () => await video_host.evaluate((v: HTMLVideoElement) => v.currentTime))
+    .toBeGreaterThanOrEqual(target)
+
+    // Host is still part of the group - host presses play, member video plays
+    await host_page.getByTestId("player-play").click()
+    await expect(video_member).toHaveJSProperty("paused", false)
+})
