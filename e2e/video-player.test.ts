4b5f708df78030cdf2308efca587efa9d81df4e8
Zithr <zithersorrow@gmail.com>
2026-04-02 03:31:28 +0100

    Revert test video

--- e2e/video-player.test.ts
+++ e2e/video-player.test.ts
@@ -11,9 +11,9 @@ type VideoInfo = {
 }
 
 const TEST_VIDEO_1: VideoInfo = {
-    url: "https://dn720709.ca.archive.org/0/items/Sintel/sintel-2048-surround.mp4",
-    length_formatted: "14:48",
-    length_s: 888.064,
+    url: "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4",
+    length_formatted: "9:56",
+    length_s: 596.458333,
 }
 
 const TEST_VIDEO_2: VideoInfo = {
